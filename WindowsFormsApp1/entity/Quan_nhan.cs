744a063ab8926a106a9129a2cd3519855ed63615
Minh Hieu <minhhieu0147896@gmail.com>
2026-03-03 22:30:20 +0700

    Add project files.

---
+++ WindowsFormsApp1/entity/Quan_nhan.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.entity
+{
+    internal class Quan_nhan
+    {
+        private int quannhan_id;
+        private string quannhan_hoten;
+        private string quannhan_capbac;
+        private string quannhan_chucvu;
+        private int donvi_id;
+        private int chedo_id;
+        private string quannhan_binhchung;
+        public Quan_nhan()
+        {
+            quannhan_id = 0;
+            quannhan_hoten = "chua xac dinh";
+            quannhan_capbac= "chua xac dinh";
+            quannhan_chucvu = "chua xac dinh";
+            donvi_id = 0;
+            chedo_id = 0;
+            quannhan_binhchung = "chua xac dinh";
+        }
+        // 3. Constructor co tham so
+        public Quan_nhan(int id, string hoten, string capbac, string chucvu, int id_donvi, int id_chedo, string binhchung)
+        {
+            quannhan_id = id;
+            quannhan_hoten = hoten;
+            quannhan_capbac = capbac;
+            quannhan_chucvu = chucvu;
+            donvi_id = id_donvi;
+            chedo_id = id_chedo;
+            quannhan_binhchung = binhchung;
+        }
+
+        // 4. Cac thuoc tinh (Properties) get/set
+        public int id
+        {
+            get { return quannhan_id; }
+            set { quannhan_id = value; }
+        }
+
+        public string hoten
+        {
+            get { return quannhan_hoten; }
+            set { quannhan_hoten = value; }
+        }
+
+        public string capbac
+        {
+            get { return quannhan_capbac; }
+            set { quannhan_capbac = value; }
+        }
+
+        public string chucvu
+        {
+            get { return quannhan_chucvu; }
+            set { quannhan_chucvu = value; }
+        }
+
+        public int id_donvi
+        {
+            get { return donvi_id; }
+            set { donvi_id = value; }
+        }
+
+        public int id_chedo
+        {
+            get { return chedo_id; }
+            set { chedo_id = value; }
+        }
+
+        public string binhchung
+        {
+            get { return quannhan_binhchung; }
+            set { quannhan_binhchung = value; }
+        }
+    }
+}
---
+++ WindowsFormsApp1/entity/User.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.entity
+{
+    internal class User
+    {
+        private int user_id;
+        private int quannhan_id;
+        private string user_taikhoan;
+        private string user_matkhau;
+        private string user_vaitro;
+        public User()
+        {
+            user_id = 0;
+            quannhan_id = 0;
+            user_taikhoan = "chua xac dinh";
+            user_matkhau = "chua xac dinh";
+            user_vaitro = "chua xac dinh";
+        }
+        // 3. Constructor co tham so
+        public User(int id, int id_quannhan, string taikhoan, string matkhau, string vaitro)
+        {
+            user_id = id;
+            quannhan_id = id_quannhan;
+            user_taikhoan = taikhoan;
+            user_matkhau = matkhau;
+            user_vaitro = vaitro;
+        }
+
+        // 4. Cac thuoc tinh (Properties) get/set
+        public int id
+        {
+            get { return user_id; }
+            set { user_id = value; }
+        }
+
+        public int id_quannhan
+        {
+            get { return quannhan_id; }
+            set { quannhan_id = value; }
+        }
+
+        public string taikhoan
+        {
+            get { return user_taikhoan; }
+            set { user_taikhoan = value; }
+        }
+
+        public string matkhau
+        {
+            get { return user_matkhau; }
+            set { user_matkhau = value; }
+        }
+
+        public string vaitro
+        {
+            get { return user_vaitro; }
+            set { user_vaitro = value; }
+        }
+    }
+}
